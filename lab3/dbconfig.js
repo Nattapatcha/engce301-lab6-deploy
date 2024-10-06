6d030771c2978cf47c54b6456049f05ebffe8122
Nattapat Chareonkithuttakorn <fixnattapat@gmail.com>
2024-10-06 14:29:14 +0700

    first commit

---
+++ lab3/dbconfig.js
@@ -0,0 +1,19 @@
+var dbconfig = {
+    development: {
+        //connectionLimit : 10,
+        host     : 'localhost',
+        port     : '3306',
+        user     : 'nattapat',
+        password : '*Fix1234',
+        database : 'moviedb'
+    },
+    production: {
+        //connectionLimit : 10,
+        host     : 'localhost',
+        port     : '3306',
+        user     : 'nattapat',
+        password : '*Fix1234',
+        database : 'moviedb'
+    }
+    };
+module.exports = dbconfig;
